8014f9602a8e9d84be25f6e748a3471d36387700
Ugis-B <54326661+Ugis-B@users.noreply.github.com>
2021-05-06 21:43:59 +0300

    kļūdu labojums nr_1

--- bank/bank/urls.py
+++ bank/bank/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+import deposits.views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', deposits.views.index),
--- bank/deposits/views.py
+++ bank/deposits/views.py
@@ -1,7 +1,7 @@
 
 from django.shortcuts import render
-from django.views.generic import View, ListView
-from user.models import User
+
+from deposits.models import Deposit
 from django.http import HttpRequest, HttpResponse
 
 def add_deposit(request):
